fa094ec4bb38ccceff0b2cc6605e79223feed9df
Elias Gomes <eliasgo221@gmail.com>
2023-08-21 15:02:41 -0300

    add funcionalidade de buscar

--- src/containers/ListaDeTarefas/styles.ts
+++ src/containers/ListaDeTarefas/styles.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-  padding: 40px 40px;
+  padding: 0 40px;
   height: 100vh;
   overflow-y: scroll;
+`
+
+export const Resultado = styled.p`
+  display: block;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  font-size: 1.125rem;
+  font-weight: bold;
 `
 // overflow-y: scroll = conteúdo que passar do tamanho definido no eixo y será mostrado com scroll
