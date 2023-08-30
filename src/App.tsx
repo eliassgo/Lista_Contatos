708870cdddd9ac155ed6150f6ce5445ea933e24c
Elias Gomes <eliasgo221@gmail.com>
2023-08-30 10:56:45 -0300

    organizante ambinte de dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import EstiloGlobal, { Container, Cores } from './styles'
-
-import store from './store'
-import Home from './page/Home'
-import Cadastro from './page/Cadastro'
+import EstiloGlobal from './styles/styles'
 
 const rotas = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
-  },
-  {
-    path: '/novo',
-    element: <Cadastro />
+    element: '/'
   }
 ])
+
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={Cores}>
-        <EstiloGlobal />
-        <Container>
-          <RouterProvider router={rotas} />
-        </Container>
-      </ThemeProvider>
-    </Provider>
+    <>
+      <EstiloGlobal />
+    </>
   )
 }
 
